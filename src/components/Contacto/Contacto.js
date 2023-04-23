5adcfc8263bba25566326c7b7baffc0f8d77ef78
Jango-7 <jhoanngo2@gmail.com>
2023-04-23 14:49:53 -0300

    update 4.4

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,30 +1,44 @@
-import { useEffect } from "react";
+import './Contacto.css'
 
 const Contacto = () => {
 
-    const clickear = (e) => {
-        console.log(e.stopPropagation())
-    }
-
-    useEffect(() => {
-
-        window.addEventListener('click', clickear)
-
-        return () => {
-            window.removeEventListener('click', clickear)
-        }
-
-    }, [])
-
-
-
 
     return (
-        <div className='container my-5' onClick={clickear}>
-            <h3>Contacto</h3>
+        <div className='container my-5'>
+            <h3 id="cont01">Contáctanos</h3>
+
+            <div class="container my-5">
+                <h1 id="contacto">Estamos para vos.</h1>
+            </div>
+
+            <div class="container formulario">
+                <div class="form-group">
+                    <label for="usr">Nombre(s):</label>
+                    <input type="text" class="form-control" id="usr"/>
+                </div>
+                <div class="form-group">
+                    <label for="usr">Apellido(s):</label>
+                    <input type="text" class="form-control" id="usr"/>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" placeholder="Enter email" id="email"/>
+                </div>
+                <div class="form-group">
+                    <label for="usr">WhatsApp:</label>
+                    <input type="text" class="form-control" id="usr"/>
+                </div>
+                <div class="form-group">
+                    <label for="comment">Tu consulta:</label>
+                    <textarea class="form-control" rows="5" id="comment"></textarea>
+                </div>
+            </div>
+            <div class="container-fluid bsubmit">
+                <button type="button" class="btn btn-outline-dark">Enviar</button>
+            </div>
 
         </div>
     )
-} 
+}
 
 export default Contacto
